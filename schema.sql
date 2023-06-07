27fd0ab797465f97ba5ccb6b05a3941bcf698be2
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-06-07 13:38:29 +0100

    the owners table, Inserting data into the species table, Update the species_id based on animal names, Update the owner_id based on owner's name

--- schema.sql
+++ schema.sql
@@ -10,5 +10,39 @@ CREATE TABLE animals
     weight_kg DECIMAL
 );
 
+-----------------------------------------------
+-----------------------------------------------
+
 ALTER TABLE animals ADD species VARCHAR(255);
 
+-----------------------------------------------
+-----------------------------------------------
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR,
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR
+);
+
+-- Add a new primary key column
+ALTER TABLE animals ADD COLUMN new_id SERIAL PRIMARY KEY;
+
+-- Drop the existing primary key constraint
+ALTER TABLE animals DROP COLUMN id;
+
+-- Rename the new primary key column to id
+ALTER TABLE animals RENAME COLUMN new_id TO id;
+
+-- Remove the species column
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add the species_id column as a foreign key referencing the species table
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+-- Add the owner_id column as a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
